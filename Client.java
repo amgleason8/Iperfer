3e07634347e701d9afc9b0c9c53f87b176dfcc25
amgleason8 <amgleason8@gmail.com>
2018-09-21 16:45:08 -0500

    Add files via upload

---
+++ Client.java
@@ -0,0 +1,17 @@
+
+public class Client {
+	
+	String name;
+	int port;
+	int time;
+	
+	Client(String name, int port, int time){
+		this.name = name;
+		this.port = port;
+		this.time = time;
+	}
+	
+	public void start() {
+		// TODO
+	}
+}
---
+++ Server.java
@@ -0,0 +1,15 @@
+
+public class Server {
+	
+	int port;
+	
+	Server(int port){
+		this.port = port;
+	}
+
+	public void start() {
+		// TODO
+		
+	}
+
+}
